715ba63c7a81d42d10aeca9edb9502bfd974289e
kehqan <154426011+kehqan@users.noreply.github.com>
2026-05-28 14:05:16 +0200

    Increase height of pd-anim-iframe to 430px

--- pilot-popup.js
+++ pilot-popup.js
@@ -228,7 +228,7 @@
     #pd-anim-iframe {
       display: block;
       width: 100%;
-      height: 310px;
+      height: 430px;
       border: none;
       margin-top: 28px;
       /* scale the 900px animation down to fit 480px panel */
